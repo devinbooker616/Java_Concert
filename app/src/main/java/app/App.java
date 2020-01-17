a7a4b7e364fa1b784e153efa484390977f89dba9
Devin Booker <dbooker@basecampcodingacademy.org>
2020-01-17 14:04:01 -0600

    completed level 1 of benchmark

--- app/src/main/java/app/App.java
+++ app/src/main/java/app/App.java
@@ -4,18 +4,50 @@ import java.util.Map;
 
 import java.util.HashMap;
 
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) {
-        Map<String, Double> m = new HashMap<String, Double>();
-        m.put("Big Theif", 5.00);
+
+    static void displayBands() {
+        final Map<String, Double> m = new HashMap<String, Double>();
+        m.put("Big Thief", 5.00);
         m.put("Gojira", 20.00);
         m.put("Secret Band", 15.00);
         m.put("Of Mice and Men", 25.00);
-        System.out.println("What band? Would you like to see?");
-
-        for (Map.Entry<String, Double> entry : m.entrySet()) {
-
+        for (final Map.Entry<String, Double> entry : m.entrySet()) {
             System.out.println(entry.getKey());
         }
+    }
+
+    static String getBand() {
+        final Scanner sc = new Scanner(System.in);
+        System.out.println("What band? Would you like to see?");
+        final Map<String, Double> m = new HashMap<String, Double>();
+        m.put("Big Thief", 5.00);
+        m.put("Gojira", 20.00);
+        m.put("Secret Band", 15.00);
+        m.put("Of Mice and Men", 25.00);
+        displayBands();
+        while (true) {
+            final String input = sc.nextLine();
+            for (final Map.Entry<String, Double> entry : m.entrySet()) {
+                if (entry.getKey().equals(input)) {
+                    System.out.println("That will be " + entry.getValue());
+                    return input;
+                } else {
+                    continue;
+                }
+            }
+        }
+    }
+
+    public static void main(final String[] args) {
+        final Scanner sc = new Scanner(System.in);
+        final Map<String, Double> m = new HashMap<String, Double>();
+        m.put("Big Thief", 5.00);
+        m.put("Gojira", 20.00);
+        m.put("Secret Band", 15.00);
+        m.put("Of Mice and Men", 25.00);
+        final String band = getBand();
     }
 }
